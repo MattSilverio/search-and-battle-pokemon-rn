f44ed07f676315c0c2b8ea45dfa00b0f5b7861f4
Matheus Silverio <matheusphillipo@gmail.com>
2024-06-24 22:11:34 -0300

    feat(*): add mock data for Pokemon battles

---
+++ src/app/mock/mocked_pockemons.js
@@ -0,0 +1,119 @@
+export const mockPokemonData = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    abilities: [
+      {
+        ability: {
+          name: "overgrow",
+          url: "https://pokeapi.co/api/v2/ability/65/",
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: "chlorophyll",
+          url: "https://pokeapi.co/api/v2/ability/34/",
+        },
+        is_hidden: true,
+        slot: 3,
+      },
+    ],
+    base_experience: 64,
+    height: 7,
+    weight: 69,
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    },
+    types: [
+      {
+        slot: 1,
+        type: { name: "grass", url: "https://pokeapi.co/api/v2/type/12/" },
+      },
+      {
+        slot: 2,
+        type: { name: "poison", url: "https://pokeapi.co/api/v2/type/4/" },
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Ivysaur",
+    abilities: [
+      {
+        ability: {
+          name: "overgrow",
+          url: "https://pokeapi.co/api/v2/ability/65/",
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: "chlorophyll",
+          url: "https://pokeapi.co/api/v2/ability/34/",
+        },
+        is_hidden: true,
+        slot: 3,
+      },
+    ],
+    base_experience: 142,
+    height: 10,
+    weight: 130,
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+    },
+    types: [
+      {
+        slot: 1,
+        type: { name: "grass", url: "https://pokeapi.co/api/v2/type/12/" },
+      },
+      {
+        slot: 2,
+        type: { name: "poison", url: "https://pokeapi.co/api/v2/type/4/" },
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Venusaur",
+    abilities: [
+      {
+        ability: {
+          name: "overgrow",
+          url: "https://pokeapi.co/api/v2/ability/65/",
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: "chlorophyll",
+          url: "https://pokeapi.co/api/v2/ability/34/",
+        },
+        is_hidden: true,
+        slot: 3,
+      },
+    ],
+    base_experience: 236,
+    height: 20,
+    weight: 1000,
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+    },
+    types: [
+      {
+        slot: 1,
+        type: { name: "grass", url: "https://pokeapi.co/api/v2/type/12/" },
+      },
+      {
+        slot: 2,
+        type: { name: "poison", url: "https://pokeapi.co/api/v2/type/4/" },
+      },
+    ],
+  },
+];
